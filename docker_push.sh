cca4aac6d1a24f1e3977c44a070697599c4919bb
Tobalase Akinyemi <funspectre@users.noreply.github.com>
2021-11-25 11:09:29 +0100

    Use correct frontend image tag

--- docker_push.sh
+++ docker_push.sh
@@ -4,7 +4,7 @@ docker tag udagram-api-feed $DOCKER_USERNAME/udagram-api-feed:v1
 docker tag reverseproxy $DOCKER_USERNAME/reverseproxy:v1
 docker tag udagram-api-feed $DOCKER_USERNAME/udagram-api-feed:v1
 docker tag udagram-api-user $DOCKER_USERNAME/udagram-api-user:v1
-docker tag udagram-frontend $DOCKER_USERNAME/udagram-frontend:v1
+docker tag udagram-frontend:local $DOCKER_USERNAME/udagram-frontend:v1
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/reverseproxy:v1
 docker push $DOCKER_USERNAME/udagram-api-feed:v1
